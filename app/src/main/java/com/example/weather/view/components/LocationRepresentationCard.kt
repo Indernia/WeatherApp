064920aa23c97ff7c9adc412c69839b647d3d56f
Jonas <s235446@dtu.dk>
2025-01-21 20:23:44 +0100

    Changed Location Cards to match theme

--- app/src/main/java/com/example/weather/view/components/LocationRepresentationCard.kt
+++ app/src/main/java/com/example/weather/view/components/LocationRepresentationCard.kt
@@ -1,12 +1,20 @@
 package com.example.weather.view.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.weather.model.LocationData
 
@@ -16,13 +24,25 @@ fun LocationRepresentationCard(
     location: LocationData,
     onClick: () -> Unit = {}
 ){
-    OutlinedCard(
-        modifier = modifier.height(40.dp),
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .padding(horizontal = 16.dp, vertical = 2.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         onClick = onClick
     ) {
-        Row {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp), // Matches the card height for consistency
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Text(
-                text = location.name
+                text = location.name,
+                textAlign = TextAlign.Center
             )
         }
     }
