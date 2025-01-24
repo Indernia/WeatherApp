074262f52ec5ce73971f04272bc6b1b779fbbeae
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2025-01-24 10:11:07 +0100

    visual update for popup

--- app/src/main/java/com/example/weather/view/popup/DeleteSavedCityPopup.kt
+++ app/src/main/java/com/example/weather/view/popup/DeleteSavedCityPopup.kt
@@ -1,14 +1,16 @@
 package com.example.weather.view.popup
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun DeleteSavedCityPopup(
@@ -18,10 +20,19 @@ fun DeleteSavedCityPopup(
 ) {
     AlertDialog(
         title = {
-            Text(text = "Delete City?")
+            Text(
+                text = "Delete City?",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium
+            )
         },
         text = {
-            Text(text = "You are deleting $city")
+            Text(
+                text = "You are deleting $city",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
         },
         onDismissRequest = {
             onDismissRequest()
